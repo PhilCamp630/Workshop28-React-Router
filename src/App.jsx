342ebcf0f206dd4b360c8e6d271bacea6b51a74a
PhilCamp630 <pcampbe8@asu.edu>
2024-08-23 15:24:45 -0700

    full screen container adjustement

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,21 @@ import Red from"./Red.jsx";
 import Blue from "./Blue.jsx";
 import Home from "./Home.jsx";
 
+
 function App() {
   return (
     <div id="container">
       <div id="navbar">
         <Link to="/">Home</Link>
-        <Link to="/blue">Blue</Link>
         <Link to="/red">Red</Link>
+        <Link to="/blue">Blue</Link>
       </div>
       <div id="main-section">
         <Routes>
           {/* Link Tags */}
           <Route path="/" element={<Home />} />
-          <Route path="/blue" element={<Blue/>} />
-          <Route path="/red" element={<Red/>} />
+          <Route path="/blue" element={<Blue />} />
+          <Route path="/red" element={<Red />} />
         </Routes>
       </div>
     </div>
